3765c6b95efabc9529b8e38f361c2b08cd1ef46f
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2024-11-06 19:32:47 +0100

    Commit Inicial - Tot fet

---
+++ MastermindSolution/Mastermind/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+namespace Mastermid
+{
+    public class Program
+    {
+        // Missatges d'Error
+        public const string ErrDifficulty = "ERROR! DIFICULTAT INVALIDA!";
+        public const string ErrOutOfRange = "ERROR! NOMBRE FORA DE RANG!";
+        public const string ErrFormatException = "ERROR! NO ÉS UN NOMBRE ENTER!";
+        public const string ErrOverflowException = "ERROR! EL NOMBRE ÉS MASSA GRAN!";
+        public const string ErrException = "ERROR INESPERAT!";
+
+        // Fletxa per a indicar quan l'usuari ha d'introduïr un valor per teclat
+        public const string InputArrow = "-> ";
+
+        public static void Main(string[] args)
+        {
+            // Codi Secret, Llargaria del Codi i Número mínim i màxim
+            const string SecretCode = "3513";
+            const int SecretCodeLenght = 4;
+            const int MinNumOfSecretCode = 1;
+            const int MaxNumOfSecretCode = 6;
+
+            // Simbols, Posició Correcte = O, Posició Incorrecte = Ø, No és en el codi = ×
+            const char SymbCorrectPosition = 'O';
+            const char SymbIncorrectPosition = 'Ø';
+            const char SymbNot = '×';
+
+            // Menú Principal, primer missatge del programa
+            const string MsgMenu =
+                "\n::::    ::::      :::      ::::::::  ::::::::::: :::::::::: :::::::::  ::::    ::::  ::::::::::: ::::    ::: :::::::::" +
+                "\n+:+:+: :+:+:+   :+: :+:   :+:    :+:     :+:     :+:        :+:    :+: +:+:+: :+:+:+     :+:     :+:+:   :+: :+:    :+:" +
+                "\n+:+ +:+:+ +:+  +:+   +:+  +:+            +:+     +:+        +:+    +:+ +:+ +:+:+ +:+     +:+     :+:+:+  +:+ +:+    +:+" +
+                "\n+#+  +:+  +#+ +#++:++#++: +#++:++#++     +#+     +#++:++#   +#++:++#:  +#+  +:+  +#+     +#+     +#+ +:+ +#+ +#+    +:+" +
+                "\n+#+       +#+ +#+     +#+        +#+     +#+     +#+        +#+    +#+ +#+       +#+     +#+     +#+  +#+#+# +#+    +#+" +
+                "\n#+#       #+# #+#     #+# #+#    #+#     #+#     #+#        #+#    #+# #+#       #+#     #+#     #+#   #+#+# #+#    #+#" +
+                "\n###       ### ###     ###  ########      ###     ########## ###    ### ###       ### ########### ###    #### #########" +
+                "\nPer: Arnau Pascual" +
+                "\n\nBenvigut a MASTERMIND!" +
+                "\nL'objectiu d'aquest joc és endevinar una combinació de 4 dígits." +
+                "\nLa combinació son nombres del 1 al 6." +
+                "\n\nEscull la dificultat:" +
+                "\n1. Dificultat novell: 10 intents" +
+                "\n2. Dificultat aficionat: 6 intents" +
+                "\n3. Dificultat expert: 4 intents" +
+                "\n4. Dificultat Màster: 3 intents" +
+                "\n5. Dificultat personalitzada";
+
+            // Separació i espai, per separar seccions i missatges
+            const string Section = "__________________________________________________\n";
+            const string Space = " ";
+
+            // Intents de cada Dificultat
+            const int DiffNovell = 10;
+            const int DiffAficionat = 6;
+            const int DiffExpert = 4;
+            const int DiffMaster = 3;
+
+            // Opcions de Dificultat, primera opció a última de la selecció de dificultats
+            const int MinDiffOptions = 1;
+            const int MaxDiffOptions = 5;
+
+            // Màxim i mínim d'intents de la Dificultat personalitzada
+            const int MinAttempts = 1;
+            const int MaxAttempts = 20;
+
+            // Missatges de resposta dirigits a l'usuari
+            const string MsgSecretQuestion = "Quin és el codi secret?";
+            const string MsgAttempt = "Intent número";
+            const string MsgYourCode = "El teu codi:";
+            const string MsgClue = "Casi! Pista:";
+            const string MsgCustomDifficulty = "Introdueix el nombre d'intents que vols tenir:";
+            const string MsgInserNum = "Introdueix el nombre de la posició";
+            const string MsgWin = "HAS GUANYAT, HAS ENDEVINAT LA COMBINACIÓ CORRECTA";
+            const string MsgLose = "HAS PERDUT, HAS ESGOTAT TOTS ELS INTENTS ¯\\_(ツ)_/¯";
+            const string MsgCorrectConvination = "La combinació correcta era";
+            const string MsgEnd = "Prem enter per finalitzar";
+
+            /* Varibles de tipus int
+             * Dificultat del joc, Intents totals, Intent actual, Total de números encertats de la combinació
+             */
+            int difficulty = 0;
+            int totalAttempts = 0;
+            int attempt = 0;
+            int correctNums = 0;
+
+            // Array per el codi que introdueix l'usuari
+            int[] arrUserSecretCode = new int[SecretCodeLenght];
+
+            /* Variables de tipus string
+             * Codi que introdueix l'usuari
+             * Conjunt de simbols (Posició Correcte, Posició Incorrecte, No està)
+             * S'utilitzen per a mostra per pantalla tots els valors junts
+             */
+            string strUserSecretCode = "";
+            string strClue = "";
+
+            // Booleà per si l'usuari encerta els 4 valors del Codi
+            bool winner = false;
+
+            Console.WriteLine(MsgMenu);
+
+            // Demana la dificultat del joc a traves de ReadLineToIntRange(min,max) amb un valor mínim i màxim
+            difficulty = ReadLineToIntRange(MinDiffOptions,MaxDiffOptions);
+
+            /* Switch que assigna els intents totals a través de la resposta de l'usuari,
+             * si la dificultat és personalitzada torna a utilitzar ReadLineToIntRange(min,max) per al nombre d'intents
+             */
+            switch (difficulty)
+            {
+                case 1:
+                    totalAttempts = DiffNovell;
+                    break;
+                case 2:
+                    totalAttempts = DiffAficionat;
+                    break;
+                case 3:
+                    totalAttempts = DiffExpert;
+                    break;
+                case 4:
+                    totalAttempts = DiffMaster;
+                    break;
+                case 5:
+                    Console.WriteLine(Section);
+                    Console.WriteLine(MsgCustomDifficulty);
+                    totalAttempts = ReadLineToIntRange(MinAttempts,MaxAttempts);
+                    break;
+                default:
+                    Console.WriteLine(ErrDifficulty);
+                    break;
+            }
+
+            /* Do While que demana els 4 nombres de la combinació amb ReadLineToIntRange(min,max),
+             * mentre l'intent actual (attempt) sigui menor al total d'intents (totalAttempts) i winner sigui fals,
+             * els nombres es guarden en arrUserSecretCode[i]
+             * Compara els 4 nombres de la combinació de l'usuari amb el Codi Secret
+             * Si un nombre està en la seva posició afegeix el valor de SymbCorrectPosition (O) a Clue (Pista)
+             * Si un nombre està en el Codi, però no en la posició afegeix el valor de SymbIncorrectPosition (Ø) a Clue (Pista)
+             * Si un nombre no hi és en la combinació afegeix el valor de SymbNot (×) a Clue (Pista)
+             * 
+             * Per cada nombre que encerta en la seva posició afegeix 1 a correctNums,
+             * si correctNums és igual a la llargaria del Codi Secret, winner és igual a true i surt del bucle,
+             * si no mostra Clue per pantalla, reinicialitza correctNums a 0 i aumenta en 1 attempt
+             */
+            do
+            {
+                Console.WriteLine(Section);
+                Console.WriteLine($"{MsgAttempt} {attempt}. {MsgSecretQuestion}");
+
+                for (int i = 0; i < SecretCodeLenght; i++)
+                {
+                    Console.WriteLine($"\n{MsgInserNum} {i + 1}");
+                    arrUserSecretCode[i] = ReadLineToIntRange(MinNumOfSecretCode,MaxNumOfSecretCode);
+                    strUserSecretCode = String.Concat(strUserSecretCode, IntToString(arrUserSecretCode[i]), Space);
+                }
+                for (int i = 0; i < SecretCodeLenght; i++)
+                {
+                    if (SecretCode.Contains(IntToChar(arrUserSecretCode[i])))
+                    {
+                        if (SecretCode[i] == IntToChar(arrUserSecretCode[i]))
+                        {
+                            strClue = String.Concat(strClue, SymbCorrectPosition, Space);
+                            correctNums++;
+                        } else
+                        {
+                            strClue = String.Concat(strClue, SymbIncorrectPosition, Space);
+                        }
+                    } else
+                    {
+                        strClue = String.Concat(strClue, SymbNot, Space);
+                    }
+                }
+
+                Console.WriteLine($"\n{MsgYourCode} {strUserSecretCode}");
+
+                if (correctNums == SecretCodeLenght)
+                {
+                    winner = true;
+                } else
+                {
+                    Console.WriteLine($"{MsgClue} {strClue}");
+                    correctNums = 0;
+                    attempt++;
+                }
+
+                strClue = "";
+                strUserSecretCode = "";
+
+            } while (attempt < totalAttempts && !winner);
+
+            Console.WriteLine(Section);
+
+            // Si winner és true mostra el missatge de victòria, si no el de derrota
+            if (winner)
+            {
+                Console.WriteLine(MsgWin);
+            } else
+            {
+                Console.WriteLine(MsgLose);
+            }
+
+            Console.WriteLine($"{MsgCorrectConvination} {SecretCode}");
+            Console.Write(MsgEnd);
+            Console.ReadLine();
+        }
+        /* Funció per a llegir un nombre per teclat a través d'un rang de nombres
+         * Utilitza ReadLineToInt(ref num, ref error) per a llegir el nombre i transformar-lo a int
+         * Amb el valor int si és dins del rang acaba la funció,
+         * si és fora del rang salta un error i torna a demanar el nombre.
+         */
+        public static int ReadLineToIntRange(int min, int max)
+        {
+            int num = 0;
+            bool error = false;
+
+            do
+            {
+                ReadLineToInt(ref num, ref error);
+                if ((num < min || num > max) && !error) Console.WriteLine(ErrOutOfRange);
+                error = false;
+            } while (num < min || num > max);
+            return num;
+        }
+        /* Funció per a llegir un nombre per teclat i intentar-lo pasar de string a int
+         * Utilitza un try per a comprovar errors i un Parse per a transformar el valor
+         * No retorna cap valor, però modifica el valor de num, una variable referenciada
+         * També, si el resultat és algún error modifica error, variable referenciada, a true
+         */
+        public static void ReadLineToInt(ref int num, ref bool error)
+        {
+            Console.Write(InputArrow);
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(ErrFormatException);
+                error = true;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(ErrOverflowException);
+                error = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ErrException);
+                error = true;
+            }
+        }
+        public static char IntToChar(int num)
+        {
+            return StringToChar(IntToString(num));
+        }
+        public static string IntToString(int num)
+        {
+            return Convert.ToString(num);
+        }
+        public static char StringToChar(string str)
+        {
+            return Convert.ToChar(str);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,184 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/I2ypp4IN)
+# Mastermind
+
+Per: **Arnau Pascual**
+
+![Portada](Img/Portada.png)
+
+## Instruccions
+
+Mastermind és un joc en el que has d'endevinar un codi de 4 nombres.
+
+Per a jugar introduïeix 4 nombres del 1 al 6, un per un. Cada cop que introdueixis 4 nombres el programa et respondrà amb una pista sobre el **Codi Secret**. A través de la pista has d'intentar endevinar el **Codi Secret**.
+
+Aquest joc té 4 dificultats establertes, a més d'una diifcultat personalitzada:
+- Dificultat novell: 10 intents
+- Dificultat aficionat: 6 intents
+- Dificultat expert: 4 intents
+- Dificultat Màster: 3 intents
+- Dificultat personalitzada: Tú esculls els intents
+
+### Pistes:
+- És en el Codi i en la seva posició -> O
+- És en el Codi, però no en la posició -> Ø
+- El nombre no és en el Codi -> ×
+
+## Explicació del funcionament del programa
+
+### Elecció de dificultat
+
+Al seleccionar una dificultat nomès podrem escriure nombres del 1 al 5, que són les opcions de dificultat que hi ha. Si introduïm un altre valor, ja sigui una lletra o un nombre o espai, retornarà un error i tornarà a demanar el valor.
+
+![ErrorDificultat](Img/ErrorDificultat.png)
+
+Aquests errors apareixen sempre que l'usuri introdueixi un nombre erroni.
+
+![ErrorCodi](Img/ErrorCodi.png)
+
+Si escollim qualsevol de les dificultats establertes jugarem amb el nombre d'intents que tenen aquestes, però si seleccionem la dificultat personalitzada haurem de posar el nombre d'intents amb els que volem jugar.
+
+![DificultatPersonalitzada](Img/DificultatPersonalitzada.png)
+
+### Endevinar el codi
+
+Un cop tinguem els intents comença el joc de veritat. El programa demanarà 4 nombres, un per un, corresponents a cada una de les posicions del Codi Secret.
+
+![CodiUsuari](Img/CodiUsuari.png)
+
+El programa contenstarà amb una pista sobre el Codi Secret.
+
+![Pista](Img/Pista.png)
+
+Si endevinem el codi abans que s'acabin els intents el programa ens felicita, en cas contrari haurem perdut. El programa sepre dona el Codi Secret al final del joc.
+
+![Victoria](Img/Victoria.png)
+![Derrota](Img/Derrota.png)
+
+## Joc de proves amb codis
+
+### Codi Secret **6543**
+
+Un codi qualsevol, per a provar una victoria i la derrota en el joc.
+
+#### Victoria
+
+![Codi1](Img/JocProves/Codi1.png)
+
+#### Derrota amb la dificultat personalitzada a 1:
+
+![Codi2-1](Img/JocProves/Codi2-1.png)
+
+#### Derrota amb la dificultat Expert:
+
+![Codi2-21](Img/JocProves/Codi2-21.png)
+
+![Codi2-22](Img/JocProves/Codi2-22.png)
+
+### Codi Secret **1346**
+
+Amb el Codi Secret **1346** comprovem els nombres mínim i màxim possibles del codi.
+
+![Codi3](Img/JocProves/Codi3.png)
+
+### Codi Secret **6421**
+
+Amb el Codi Secret **6421** comprovem els nombres mínim i màxim possibles del codi.
+
+![Codi4](Img/JocProves/Codi4.png)
+
+### Codi Secret **2526**
+
+Amb el Codi Secret **2526** comprovem dos nombres iguals en el codi separats.
+
+![Codi5](Img/JocProves/Codi5.png)
+
+### Codi Secret **3411**
+
+Amb el Codi Secret **3411** comprovem dos nombres iguals en el codi en posicions seguides.
+
+![Codi6](Img/JocProves/Codi6.png)
+
+### Codi Secret **4444**
+
+Amb el Codi Secret **4444** comprovem tots els nombres iguals en el codi.
+
+![Codi7](Img/JocProves/Codi7.png)
+
+### Codi Secret **6666**
+
+Amb el Codi Secret **6666** comprovem tots els nombres iguals en el codi i el valor màxim que pot tenir el codi.
+
+![Codi8](Img/JocProves/Codi8.png)
+
+## Punts entregats
+
+### Robust
+
+El programa s'executa sense errors i no apareix cap tracer per pantalla i fa el que es demana.
+
+Dins del programa pots seleccionar una dificultat, cada una té un nombre d'intents diferent, a més d'una dificultat personalitzada per a seleccionar els intents personalment. Un cop definits els intents el programa compara un codi introduït per l'usuari amb el que té guardat el programa, això es repeteix fins que els intents de l'usuari s'acabin, en aquest cas, si els no l'ha endevinat en l'últim intent el programa s'acaba amb una derrota.
+
+![Robust](Img/Criteris/Robust.png)
+
+### Claredat i ordre
+
+El programa es segueix amb claredat, de dalt a baix, cada ordre va seguida d'una altra. Les instruccions són suficients clares com per entendre-les.
+
+Tot el codi utilitzat per al programa l'hem treballat a classe.
+
+![Claredat1](Img/Criteris/Claredat1.png)
+
+Les funcions es criden de dalt a baix, qui la crida a dalt, i la funció abaix. També aquestes només realitzen una funció.
+
+![Claredat2](Img/Criteris/Claredat2.png)
+
+Els comentaris està col·locats per a explicar el funcionament d'algunes parts del codi.
+
+![Claredat3](Img/Criteris/Claredat3.png)
+
+### Variables i constants
+
+Totes les variables i constats del programa tenen noms que nomès en saber el seu nom es pot deduïr per a que serveixen, a més de que tot està escrit en anglès.
+
+![Variables1](Img/Criteris/Variables1.png)
+
+Tots els textos que utilitza el programa està declarats en constats, no hi ha cap text que s'utilitzu que no estigui dintre d'una.
+
+![Variables2](Img/Criteris/Variables2.png)
+
+Totes les variables i constats està declarades al principi del codi.
+
+![Variables3](Img/Criteris/Variables3.png)
+
+### Estructures de control
+
+Totes les condicions del codi són correctes i no provoquen errors en el programa. Aquestes sempre tenen algún ús dins del codi.
+
+![Estructures](Img/Criteris/Estructures.png)
+
+L'única sentència break que s'utilitza està en un switch.
+
+### Gestió d'errors
+
+Tots els errors que es pot produïr està controlats.
+
+![Errors](Img/Criteris/Errors.png)
+
+### Disseny modular
+
+Els processos que es repeteixen més d'una vegada estàn dins de mètodes per a no haver de repetir el codi.
+
+![Disseny](Img/Criteris/Disseny.png)
+
+### UX/UI
+
+Els missatges ajuden a entendre millor el joc, i encaminen a l'usuari a jugar correctament.
+
+![UI](Img/Criteris/UI.png)
+
+### Extra 3
+
+El codi utilitza un títol gran i llegible per una major inmersió al joc, a més hi ha separacions per a llegirr millor cada part del programa.
+
+![Extra1](Img/Criteris/Extra1.png)
+
+![Extra2](Img/Criteris/Extra2.png)
